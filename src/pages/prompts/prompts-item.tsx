2943ab7e9d1074954188c33975790a8836706a0e
Mustafa Ihssan A. Naji <mustafa.ihssan@gmail.com>
2021-04-02 13:15:06 +0300

    prompts redesign (#3)

--- src/pages/prompts/prompts-item.tsx
+++ src/pages/prompts/prompts-item.tsx
@@ -8,23 +8,95 @@ interface Props {
 	tags: string[]
 }
 
+const getRandRange = () => Math.floor(Math.random() * 100)
+
 export function PromptsItem(item: Props) {
+	const tempImages = [...Array(3).fill(0)].map(
+		(i) =>
+			`https://randomuser.me/api/portraits/${
+				i % 2 === 0 ? 'men' : 'women'
+			}/${getRandRange()}.jpg`
+	)
+
 	return (
-		<div className="bg-pink-200 text-center p-5 m-2">
-			<ul>
-				<li>Language: {item.language}</li>
-				<li className="text-xl my-6">{item.content}</li>
-				<div className="space-x-2 m-5">
-					{item.tags?.map((i) => (
-						<span className="p-2 bg-blue-200" key={item.id + i}>
-							{i}
-						</span>
+		<div className="bg-gray-100 text-center px-5 py-8 m-5 rounded-lg shadow">
+			<div className="space-x-2 m-5">
+				{item.tags?.map((i) => (
+					<span className="p-2 rounded bg-blue-200" key={item.id + i}>
+						{i.toUpperCase()}
+					</span>
+				))}
+				<span className="p-2 bg-blue-200">
+					{item.language.toUpperCase()}
+				</span>
+			</div>
+
+			<p
+				className={`font-sans subpixel-antialiased tracking-wide leading-relaxed
+				mt-8 px-5 text-xl ${
+					item.language === 'en' ? 'text-left' : 'text-right'
+				} text-gray-900`}
+			>
+				{item.content}
+			</p>
+
+			<div className="flex flex-row space-x-2 items-center">
+				<div className="flex justify-start -space-x-2 pl-5 py-5">
+					{tempImages.map((i) => (
+						<img
+							key={i}
+							src={i}
+							alt="user.name"
+							width="64"
+							height="64"
+							className="w-12 h-12 rounded-full bg-gray-100 border-2 border-white"
+						/>
 					))}
 				</div>
-			</ul>
-			<Link to={`/prompts/${item.id}`}>
-				<Button>View</Button>
-			</Link>
+				<p className="text-gray-500 mx-auto h-full">
+					Connie and other Posted here.
+				</p>
+			</div>
+
+			<div className="flex border-t p-3">
+				<Link to={`/prompts/${item.id}`}>
+					<div className="flex rounded-lg space-x-2 p-3 hover:bg-blue-100 hover:text-blue-700 text-gray-700">
+						<svg
+							className="w-6 h-6 text-gray-500"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M5 11l7-7 7 7M5 19l7-7 7 7"
+							/>
+						</svg>
+						<span>25 Like</span>
+					</div>
+				</Link>
+
+				<div className="flex rounded-lg space-x-2 p-3 hover:bg-blue-100 hover:text-blue-700 text-gray-700">
+					<svg
+						className="w-6 h-6 text-gray-500"
+						fill="none"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+						/>
+					</svg>
+					<span>6 Post</span>
+				</div>
+			</div>
 		</div>
 	)
 }
